ae839cd50486bd36d4798cf6a4816793364e26b0
Teodor G <grigor.teodor@gmail.com>
2020-08-20 19:53:55 +0300

    updated content

---
+++ include - Copy/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database configuration
+ */
+
+/**
+ * data encrypted via https://codebeautify.org/encrypt-decrypt
+ * only @TeodorHMX1 has access to the encryption key
+ * ask him to provide it only if you are a member of the ALMT/Quest-Mode Team
+ * 
+ * Alghoritm: Arcfour
+ * Mode: CBC
+ */
+define('DB_USERNAME', 'id13957036_iggetprodb');
+define('DB_PASSWORD', '~D(nF8FEn0AM7A4%[oKP3W=ksZT#c=Je');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'id13957036_iggetpro');
+
+?>
